fec33a046a2bfecdbb40a651761502bf5785cd13
hnagd1 <hudsongraylaw@gmail.com>
2026-09-13 00:51:40 -0700

    Test php code because I needed something to upload

---
+++ phptest.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "Wow, you found our secret php testing page. Well done.\n\n\nOr maybe you just found this on our github. Either way, you probably spent more time interacting with our site than the average person.";
+?>
+
+</body>
+</html>
